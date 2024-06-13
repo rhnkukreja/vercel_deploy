c2ba53dbde0d571e8a6cabdb06b63c1814bfcd8c
rhnkukreja <94891842+rhnkukreja@users.noreply.github.com>
2024-06-13 19:10:46 +0530

    Update app.py
    Added the entire conversation flow to the APP

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = FastAPI()
 
 @app.get("/text_response")
 async def text_response(request: Request):
-    return PlainTextResponse("This is a plain text response from your                                   FastAPI backend.")
+    return PlainTextResponse("""Hello, how can i assist you today?                            
+                                What type of room would you like, we have standard and executive room type                                                                                    
+                                Yes, we do. The price is 4000 per night.                                                                                    
+                                Anything else i can help you with?                                                                                    
+                                Have a Good day
+                                """)
 
 if __name__ == "__main__":
     import uvicorn
