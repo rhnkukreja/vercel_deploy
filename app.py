a0978f630b32896636c16ce565fc0e6361f9767b
rhnkukreja <94891842+rhnkukreja@users.noreply.github.com>
2024-07-02 15:41:44 +0530

    Update app.py
    Changes to the format of the script

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file for local development
 load_dotenv()
 
+
 app = FastAPI()
 
 # Set environment variables for your credentials
